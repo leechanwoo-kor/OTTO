33b6eb9e16b35390052aa5d46be50691a36bd5ed
Charles Lee <cwlee1124@gmail.com>
2023-01-14 03:09:15 +0900

    :memo: Create test.py.

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from glob import glob
+
+files = sorted(glob('./train_parquet/*'))
+m_df = pd.DataFrame()
+
+for path in files:
+    df = pd.read_parquet(path)
+    g_df = df.groupby('session').agg(list).reset_index()
+    m_df = pd.concat([m_df, g_df])
